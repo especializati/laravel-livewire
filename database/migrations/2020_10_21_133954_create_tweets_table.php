6dbb36b1ebaf79a5c0d38e446d460b4662eaee29
Carlos Ferreira <conta@especializati.com.br>
2020-10-21 10:56:14 -0300

    Create Model and Migration Tweet

---
+++ database/migrations/2020_10_21_133954_create_tweets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('content');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                        ->references('id')
+                        ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tweets');
+    }
+}
