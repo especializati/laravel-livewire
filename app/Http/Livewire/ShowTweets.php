4ff2870007f83cbd733bb9a8e0d1f00db34b9fac
Carlos Ferreira <conta@especializati.com.br>
2020-10-21 11:45:49 -0300

    Validations

--- app/Http/Livewire/ShowTweets.php
+++ app/Http/Livewire/ShowTweets.php
@@ -7,7 +7,11 @@ use Livewire\Component;
 
 class ShowTweets extends Component
 {
-    public $message = 'Apenas um teste 2';
+    public $content = 'Apenas um teste 2';
+
+    protected $rules = [
+        'content' => 'required|min:3|max:255'
+    ];
 
     public function render()
     {
@@ -20,11 +24,13 @@ class ShowTweets extends Component
 
     public function create()
     {
+        $this->validate();
+
         Tweet::create([
-            'content' => $this->message,
+            'content' => $this->content,
             'user_id' => 1,
         ]);
 
-        $this->message = '';
+        $this->content = '';
     }
 }
--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,10 +1,11 @@
 <div>
     Show Tweets
 
-    <p>{{ $message }}</p>
+    <p>{{ $content }}</p>
 
     <form method="post" wire:submit.prevent="create">
-        <input type="text" name="message" id="message" wire:model="message">
+        <input type="text" name="content" id="content" wire:model="content">
+        @error('content') {{ $message }} @enderror
         <button type="submit">Criar Tweet</button>
     </form>
 
