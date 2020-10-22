6dfabea5564c8a31182d6911226d7bf435479637
Carlos Ferreira <conta@especializati.com.br>
2020-10-22 10:52:23 -0300

    Create Tweet with user authenticated

--- app/Http/Livewire/ShowTweets.php
+++ app/Http/Livewire/ShowTweets.php
@@ -18,7 +18,7 @@ class ShowTweets extends Component
 
     public function render()
     {
-        $tweets = Tweet::with('user')->paginate(2);
+        $tweets = Tweet::with('user')->latest()->paginate(10);
 
         return view('livewire.show-tweets', [
             'tweets' => $tweets
@@ -29,9 +29,8 @@ class ShowTweets extends Component
     {
         $this->validate();
 
-        Tweet::create([
+        auth()->user()->tweets()->create([
             'content' => $this->content,
-            'user_id' => 1,
         ]);
 
         $this->content = '';
--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function tweets()
+    {
+        return $this->hasMany(Tweet::class);
+    }
 }
